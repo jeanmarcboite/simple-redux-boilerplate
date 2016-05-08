04de21067032b6f54da9a80a0cc151ec7883117a
Jean-Marc Boite <jeanmarcboite@gmail.com>
2016-05-08 12:53:36 +0200

    parameterized loops, cleaned

--- src/components/HandDistribution/index.js
+++ src/components/HandDistribution/index.js
@@ -13,92 +13,85 @@ function add_a_shorter_suit(hand) {
 
 function numberOfEqualSuitLength(hand) {
   return hand.reduce(function(dCount, currentD){
-      if(typeof dCount[currentD] !== "undefined"){
-        dCount[currentD]++;
-        return dCount;
-      } else {
-          dCount[currentD]=1;
-          return dCount;
-      }
+    if(typeof dCount[currentD] !== "undefined"){
+      dCount[currentD]++;
+      return dCount;
+    } else {
+      dCount[currentD]=1;
+      return dCount;
+    }
   }, {});
 }
 
 
 class HandDistribution extends React.Component {
-  isSorted = (array) => {
-    for (var i = 0; i < array.length - 1; i++)
-    if (array[i] < array[i + 1])
-    return false;
-    return true;
-  }
 
-
-  tableRow = () => {
+  tableRows = () => {
     var data = [];
     const total = math.combinations(this.props.suit.count * this.props.suit.length, this.props.hand.length);
 
-// start with arrays of one suit
-var distributions = _.range(this.props.suit.length, 0).map(a => [a])
-// replace arrays with arrays of one suit more
-while (distributions[0].length < this.props.suit.count) {
-  distributions = _.flatMap(distributions.map(add_a_shorter_suit))
-}
-// filter distributions with wrong number of cards
-_.remove(distributions, el => _.reduce(el, (sum, n) => sum + n, 0) != this.props.hand.length);
+    // start with arrays of one suit
+    var distributions = _.range(this.props.suit.length, 0).map(a => [a])
+    // replace arrays with arrays of one suit more
+    while (distributions[0].length < this.props.suit.count) {
+      distributions = _.flatMap(distributions.map(add_a_shorter_suit))
+    }
+    // filter distributions with wrong number of cards
+    _.remove(distributions, el => _.reduce(el, (sum, n) => sum + n, 0) != this.props.hand.length);
 
     for (var d of distributions) {
-// find number of duplicates
-            var dCount = numberOfEqualSuitLength(d)
-            // divide by prod of factorial
-            var div = (_.map(dCount, (x) => math.factorial(x))).reduce((p, y) => (p*y), 1);
-            // number of permutations of the suits
-            var m = math.divide(math.factorial(this.props.suit.count), div);
-            // number of permutations for that specific distribution
-            var combinations = d.map(x => math.combinations(this.props.suit.length, x)).reduce((p, y) => math.multiply(p, y), 1)
-            // total number of combinations: multiply by the permutation count
-            var cc = math.multiply(combinations, m);
+      // find number of duplicates
+      var dCount = numberOfEqualSuitLength(d)
+      // divide by prod of factorial
+      var div = (_.map(dCount, (x) => math.factorial(x))).reduce((p, y) => (p*y), 1);
+      // number of permutations of the suits
+      var m = math.divide(math.factorial(this.props.suit.count), div);
+      // number of permutations for that specific distribution
+      var combinations = d.map(x => math.combinations(this.props.suit.length, x)).reduce((p, y) => math.multiply(p, y), 1)
+      // total number of combinations: multiply by the permutation count
+      var cc = math.multiply(combinations, m);
 
-          data.push(_.concat(d, [math.divide(math.multiply(cc, 100), total), cc, m]));
+      data.push(_.concat(d, [math.divide(math.multiply(cc, 100), total), cc, m]));
+    }
+
+    return data;
   }
 
-  return data;
-}
+  render = () => {
+    const centerStyle = {
+      textAlign: 'center'
+    }
+    const rightStyle = {
+      textAlign: 'right'
+    }
+    var key = 0;
 
-render = () => {
-  const centerStyle = {
-    textAlign: 'center'
-  }
-  const rightStyle = {
-    textAlign: 'right'
-  }
-  var key = 0;
-
-  return (
-    <div class="container">
-      <Table striped bordered condensed hover>
-        <caption><h2>Hand distribution probabilities</h2></caption>
-        <thead>
-          <tr>
-            <th colSpan={this.props.suit.count} style={centerStyle}>Distribution</th>
+    return (
+      <div class="container">
+        <Table striped bordered condensed hover>
+          <caption><h2>Hand distribution probabilities</h2></caption>
+          <thead>
+            <tr>
+              <th colSpan={this.props.suit.count} style={centerStyle}>Distribution</th>
               <th style={centerStyle}>Probability</th>
-            <th style={centerStyle}>Combinations</th>
-          </tr>
-        </thead>
-        <tbody id="table-body">
-          {this.tableRow().map(item => (<tr key={key++}>{item.map(el => (<td key={key++} style={rightStyle}>{el}</td>))}</tr>))}
-        </tbody>
-      </Table>
-    </div>);
+              <th style={centerStyle}>Combinations</th>
+            </tr>
+          </thead>
+          <tbody id="table-body">
+            {this.tableRows().map(item => (<tr key={key++}>{item.map(el => (<td key={key++} style={rightStyle}>{el}</td>))}</tr>))}
+          </tbody>
+        </Table>
+      </div>);
+    }
   }
-}
 
-function mapStateToProps(state) {
-  return {
-    suit: {count: 4, length: 13},
-    hand: {count: 4, length: 13}
-  };
-}
+  function mapStateToProps(state) {
+    return {
+      suit: {count: 4, length: 13},
+      hand: {count: 4, length: 13}
+    };
+  }
 
-export default connect(
-  mapStateToProps
-)(HandDistribution);
+  export default connect(
+    mapStateToProps
+  )(HandDistribution);
